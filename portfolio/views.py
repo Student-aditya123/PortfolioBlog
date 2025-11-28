3778d40efde6ad0d0ba9f236ce1d17a843a9fea6
Aditya Singh <adityasingh606104@gmail.com>
2025-11-28 21:20:36 +0530

    add portfolio pages, project model, and contact form

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import Project
+from .forms import ContactForm
 # Create your views here.
+
+def home(request):
+    return render(request, 'portfolio/home.html')
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/projects.html', {'projects': projects})
+
+def contact(request):
+    form = ContactForm()
+    return render(request, 'portfolio/contact.html', {'form': form})
--- portfolio_blog/settings.py
+++ portfolio_blog/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'portfolio_blog.wsgi.application'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 # Database
