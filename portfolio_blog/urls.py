73dea95c58aad160da66a2c5923dc6c785cd1b56
Aditya Singh <adityasingh606104@gmail.com>
2026-02-03 13:52:18 +0530

    contact form

--- portfolio_blog/urls.py
+++ portfolio_blog/urls.py
@@ -21,8 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
-    # path('portfolio/', include('portfolio.urls')),
-    # path("blog/", include("blog.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 
 ]
--- portfolio_blog/views.py
+++ portfolio_blog/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+   
 def home(request):
-    return render(request, 'websites/index.html')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        print("NAME:", name)
+        print("EMAIL:", email)
+        print("MESSAGE:", message)
+        
+        # Django popup message
+        messages.success(request, "✅ Message sent successfully!")
+
+    return render(request, "websites/index.html")
+
+
+# def home(request):
+#     return render(request, 'websites/index.html')
