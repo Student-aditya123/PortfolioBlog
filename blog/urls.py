a558896518d253467dd41c8d5520f90e35523467
Aditya Singh <adityasingh606104@gmail.com>
2026-02-03 13:09:47 +0530

    delete all unnecessary files from project

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-urlpatterns = [
-     path('blog_list/', views.blog_list, name='blog_list'),
-     path('blog_detail/', views.blog_detail, name='blog_detail'),
-]
